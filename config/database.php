43a9a0a9c955628b2096ff56850507e79ea97b1f
VrainsieTech <vickessyrainson@gmail.com>
2025-01-18 18:53:11 +0300

    Added Postgress

--- config/database.php
+++ config/database.php
@@ -1,12 +1,30 @@
 <?php
 
 return [
-    'connections' => [
-        'mysql' => [
-            'host' => getenv('DB_HOST', 'localhost'),
-            'database' => getenv('DB_DATABASE'),
-            'username' => getenv('DB_USERNAME'),
-            'password' => getenv('DB_PASSWORD'),
-        ],
-    ],
+        'default' => getenv('DB_CONNECTION', 'mysql'),
+        'connections' => [
+            'mysql' => [
+                'host' => getenv('DB_HOST', 'localhost'),
+                'port' => getenv('DB_PORT', 3306),
+                'database' => getenv('DB_DATABASE'),
+                'username' => getenv('DB_USERNAME'),
+                'password' => getenv('DB_PASSWORD'),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
+            'pgsql' => [ // Example for Postgres
+                'host' => getenv('PGSQL_HOST', 'localhost'),
+                'port' => getenv('PGSQL_PORT', 5432),
+                'database' => getenv('PGSQL_DATABASE'),
+                'username' => getenv('PGSQL_USERNAME'),
+                'password' => getenv('PGSQL_PASSWORD'),
+                'charset' => 'utf8',
+                'prefix' => '',
+                'schema' => 'public',
+                'sslmode' => 'prefer',
+            ],
+        ]
 ];
