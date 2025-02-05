27c718131a11a04041d01fc7a2f5240c8e9e9133
VrainsieTech <vickessyrainson@gmail.com>
2025-02-05 10:14:48 +0300

    Fixed Connection issues

--- src/Models/DbTables.php
+++ src/Models/DbTables.php
@@ -1,13 +1,13 @@
 <?php
 namespace Vrainsitech\Vrtmvc\Models;
-use Vrainsietech\Vrtmvc\VrtDb;
+use Vrainsietech\Vrtmvc\Core\VrtDb;
 
 /**
  * Database Tables.
  * 
  * Provide sql file with tables or create table(s) manually. Pass in the file or the sql create table statement. This function will create the table for you in the database already created. So, it is wise to call this method after you have first called the dbCreate.
  * 
- * @param string $tables sql file name or path. File must only contain sql create table statements
+ * @param string $tables sql file name or path. File must only contain sql table statements
  * 
  * @return boolean true or false depending with outcome, ofcourse true on success
  * 
@@ -17,32 +17,34 @@ use Vrainsietech\Vrtmvc\VrtDb;
 
 class dbTables extends VrtDb {
 	private $tables;
+    private $vrt;
 
-	function __construct($tables){
+	function __construct($tables, VrtDb $vrt){
 		$this->table = $tables;
+        $this->vrt = $vrt;
 	}
 
 	function createTables(){
 
 
-	if (!file_exists($sqlFilePath)) {
+	if (!file_exists($this->table)) {
        throw new Exception("SQL file not found: " . $this->table);
     }
 
     $sql = file_get_contents($this->table);
 
-        if ($this->vrtdb()->multi_query($sql)) {
+        if ($this->$vrt->vrtdb()->multi_query($sql)) {
             do {
-                if ($result = $this->vrtdb()->store_result()) {
+                if ($result = $this->$vrt->vrtdb()->store_result()) {
                     $result->free();
                 }
-                if (!$this->vrtdb()->more_results()) {
+                if (!$this->$vrt->vrtdb()->more_results()) {
                     break;
                 }
-            } while ($this->vrtdb()->next_result());
+            } while ($this->$vrt->vrtdb()->next_result());
             return true;
         } else {
-            throw new Exception("Error executing SQL: " . $this->vrtdb()->error);
+            throw new Exception("Error executing SQL: " . $this->$vrt->vrtdb()->error);
         }
 
     }
