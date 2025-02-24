9a1e69a62e25f3f84f0dcf042048165653557561
VrainsieTech <vickessyrainson@gmail.com>
2025-02-24 12:47:48 +0300

    Fixed Constant names to match .env

--- config/database.php
+++ config/database.php
@@ -6,16 +6,16 @@ return [
             'mysql' => [
                 'host' => getenv('DB_HOST', 'localhost'),
                 'port' => getenv('DB_PORT', 3306),
-                'database' => getenv('DB_DATABASE'),
-                'username' => getenv('DB_USERNAME'),
-                'password' => getenv('DB_PASSWORD'),
+                'database' => getenv('DB_NAME'),
+                'username' => getenv('DB_USER'),
+                'password' => getenv('DB_PASS'),
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
                 'prefix' => '',
                 'strict' => true,
                 'engine' => null,
             ],
-            'pgsql' => [ // Example for Postgres
+            'pgsql' => [
                 'host' => getenv('PGSQL_HOST', 'localhost'),
                 'port' => getenv('PGSQL_PORT', 5432),
                 'database' => getenv('PGSQL_DATABASE'),
